fcafd6884f2154a35af8a5cbbee3b21fc9353944
Denis Kapitulin <kapitulin24@gmail.com>
2021-03-05 00:32:35 +0300

    added feedback section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,20 @@
-$('ul.species__tabs-name').on('click', 'li:not(.species__tab_active)', function() {
-    $(this)
-        .addClass('species__tab_active').siblings().removeClass('species__tab_active')
-         .closest('div.species__tabs').find('div.species__content').removeClass('species__content_active').eq($(this).index()).addClass('species__content_active');
-  });
+$(document).ready(function(){
+    $('ul.species__tabs-name').on('click', 'li:not(.species__tab_active)', function() {
+        $(this)
+            .addClass('species__tab_active').siblings().removeClass('species__tab_active')
+            .closest('div.species__tabs').find('div.species__content').removeClass('species__content_active').eq($(this).index()).addClass('species__content_active');
+    });
+
+    $('.feedback__sliders').slick({
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrows/arrow_left.svg" alt=""></button> <div class="slick-prev-descr">Смотреть<br> предыдущий отзыв</div>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows/arrow_right.svg" alt=""></button> <div class="slick-next-descr">Смотреть<br> следующий отзыв</div>',
+        dots: true,
+	});
+    $('.feedback__sliders').on('setPosition', function(){
+        const changeCount = document.querySelector('.feedback__counter'),
+              currentSlide = $(".feedback__sliders").slick("slickCurrentSlide"),
+              countFeedback = document.querySelectorAll('[role="tabpanel"]');
+
+        changeCount.textContent = `Отзыв ${currentSlide + 1} из ${countFeedback.length}`;
+      });
+});

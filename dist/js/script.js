4b6fa0ac36f0d56d5b76c2ffff4eb08ccbf7624f
Denis Kapitulin <kapitulin24@gmail.com>
2021-03-16 00:27:46 +0300

    added mobile adaptation for header, promo and choice summer sections

--- dist/js/script.js
+++ dist/js/script.js
@@ -25,4 +25,33 @@ $(document).ready(function(){
         infinite: false,
         variableWidth: true
 	});
+
+    const menu = document.querySelector('.header-content__navigation'),
+          menuItem = document.querySelectorAll('.header-content__navigation ul li'),
+          hamburger = document.querySelector('.header__hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('open');
+        menu.classList.toggle('header-content__navigation_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('open');
+            menu.classList.toggle('header-content__navigation_active');
+        })
+    });
+
+    const getLink = document.querySelector('button.modal__get-link'),
+          showBtn = document.querySelector('button.modal__btn'),
+          showInpt = document.querySelectorAll('.modal input');
+
+    getLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        getLink.style = 'display: none';
+        showBtn.style = 'display: block';
+        showInpt.forEach((e) => {
+            e.style = 'display: block';
+        });
+    });
 });
